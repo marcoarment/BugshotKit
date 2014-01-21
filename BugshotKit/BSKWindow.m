0cd1d8da47802324b83d80ad54372b5e2973dad8
Bryan Irace <bryan.irace@gmail.com>
2014-01-21 10:47:29 -0500

    Add support for launching Bugshot via shake event

---
+++ BugshotKit/BSKWindow.m
@@ -0,0 +1,18 @@
+//  BSKWindow.m
+//  See included LICENSE file for the (MIT) license.
+//  Created by Bryan Irace on 1/21/14.
+
+#import "BSKWindow.h"
+#import "BugshotKit.h"
+
+@implementation BSKWindow
+
+#pragma mark - UIResponder
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (event.subtype == UIEventSubtypeMotionShake) {
+        [BugshotKit show];
+    }
+}
+
+@end
