66ad6ff629c44c87203c57abd173d97ee1ca1607
Marco Arment <me@marco.org>
2014-05-16 12:05:07 -0400

    When using shake, fixed BugshotKit being shown immediately when the app is reactivated after being locked

--- BugshotKit/BSKWindow.m
+++ BugshotKit/BSKWindow.m
@@ -5,14 +5,41 @@
 #import "BSKWindow.h"
 #import "BugshotKit.h"
 
+
+@interface BSKWindow ()
+@property (nonatomic) NSTimeInterval applicationActivatedAtTime;
+@end
+
 @implementation BSKWindow
 
 #pragma mark - UIResponder
 
-- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if ( (self = [super initWithFrame:frame]) ) {
+        [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(applicationDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [NSNotificationCenter.defaultCenter removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
+}
+
+- (void)applicationDidBecomeActive:(NSNotification *)n
+{
+    self.applicationActivatedAtTime = NSDate.date.timeIntervalSince1970;
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
     [super motionEnded:motion withEvent:event];
 
-    if (event.subtype == UIEventSubtypeMotionShake) {
+    if (event.subtype == UIEventSubtypeMotionShake &&
+        UIApplication.sharedApplication.applicationState == UIApplicationStateActive &&
+        NSDate.date.timeIntervalSince1970 - self.applicationActivatedAtTime > 1.0
+    ) {
         [BugshotKit show];
     }
 }
